60144410f5b606b72664e66c9eb156238513de8f
Laura Kelly <laurakelly135@gmail.com>
2014-07-07 18:30:09 -0700

    part_2 finished. bar chart done

--- part_2/data_vis.js
+++ part_2/data_vis.js
@@ -1,11 +1,53 @@
 (function () {
-    var margin = {top: 20, right: 20, bottom: 20, left: 20},
-        width: = 960 - margin.left - margin.right,
-        height = 960 - margin.top - margin.bottom;
+    var margin = {top: 20, right: 40, bottom: 40, left: 40},
+        width = 500 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom;
     
-    var x = d3.scale.ordinal(),
-            .range([0, 120]);
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .2, .15);
 
-    var barchart = d3.select("#barchart");
+    var y = d3.scale.linear()
+            .range([height, 0]);
 
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    var barchart = d3.select("#barchart").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.csv("data/data.csv", function(error, data) {
+        x.domain(data.map(function(d) { return d.city }));
+        y.domain([0, d3.max(data, function(d){ return parseFloat(d.average) + 10 }) ]);
+
+        barchart.append("g")
+            .attr("class", "xAxis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        barchart.append("g")
+            .attr("class", "yAxis")
+            .call(yAxis)
+          .append("text");
+        
+        barchart.selectAll(".bar")
+            .data(data)
+          .enter().append("rect")
+            .attr("class", function (d) {
+                var city = d.city.replace(/\s+/g, '-').toLowerCase();
+                return "bar " + city   
+            })
+            .attr("x", function(d) { return x(d.city); })
+            .attr("width", x.rangeBand())
+            .attr("y", function(d) { return y(d.average) })
+            .attr("height", function (d) { return height - y(d.average); });
+            
+    });
 })();
